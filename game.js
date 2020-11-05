fc9c237df3901295de8a4dfa01869c3d37ffa913
smrk <rodriguezsm72@gmail.com>
2020-11-05 19:25:44 -0300

    Snake-Interacting with other elements

--- game.js
+++ game.js
@@ -7,25 +7,30 @@ var KEY_ENTER = 13,
     ctx = null,
     lastPress = null,
     pause = true,
-    x = 50,
-    y = 50,
-    dir = 0;
+    dir = 0,
+    score = 0,
+    player = null,
+    food = null;
 
 window.requestAnimationFrame = (function() { return window.requestAnimationFrame || window.mozRequestAnimationFrame || window.webkitRequestAnimationFrame || function(callback) { window.setTimeout(callback, 17); }; }());
 document.addEventListener('keydown', function(evt) { lastPress = evt.which; }, false);
-window.addEventListener('load', init, false);
 
 function paint(ctx) {
-    // Clean canvas     
+    // Clean canvas 
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // Draw square     
+    // Draw player  
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(x, y, 10, 10);
-    // Debug last key pressed    
+    player.fill(ctx);
+    // Draw food   
+    ctx.fillStyle = '#f00';
+    food.fill(ctx);
+    // Debug last key pressed   
     ctx.fillStyle = '#fff';
-    //ctx.fillText('Last Press: ' + lastPress, 0, 20);
-    // Draw pause     
+    //ctx.fillText('Last Press: '+lastPress,0,20);  
+    // Draw score 
+    ctx.fillText('Score: ' + score, 0, 10);
+    // Draw pause 
     if (pause) {
         ctx.textAlign = 'center';
         ctx.fillText('PAUSE', 150, 75);
@@ -34,25 +39,29 @@ function paint(ctx) {
 }
 
 function act() {
-
     if (!pause) {
-        // Change Direction        
+        // Change Direction 
         if (lastPress == KEY_UP) { dir = 0; }
         if (lastPress == KEY_RIGHT) { dir = 1; }
         if (lastPress == KEY_DOWN) { dir = 2; }
         if (lastPress == KEY_LEFT) { dir = 3; }
-        // Move Rect       
-        if (dir == 0) { y -= 10; }
-        if (dir == 1) { x += 10; }
-        if (dir == 2) { y += 10; }
-        if (dir == 3) { x -= 10; }
-        // Out Screen       
-        if (x > canvas.width) { x = 0; }
-        if (y > canvas.height) { y = 0; }
-        if (x < 0) { x = canvas.width; }
-        if (y < 0) { y = canvas.height; }
-    }
-    // Pause/Unpause     
+        // Move Rect    
+        if (dir == 0) { player.y -= 10; }
+        if (dir == 1) { player.x += 10; }
+        if (dir == 2) { player.y += 10; }
+        if (dir == 3) { player.x -= 10; }
+        // Out Screen   
+        if (player.x > canvas.width) { player.x = 0; }
+        if (player.y > canvas.height) { player.y = 0; }
+        if (player.x < 0) { player.x = canvas.width; }
+        if (player.y < 0) { player.y = canvas.height; }
+        // Food Intersects    
+        if (player.intersects(food)) {
+            score += 1;
+            food.x = random(canvas.width / 10 - 1) * 10;
+            food.y = random(canvas.height / 10 - 1) * 10;
+        }
+    } // Pause/Unpause   
     if (lastPress == KEY_ENTER) {
         pause = !pause;
         lastPress = null;
@@ -70,10 +79,25 @@ function run() {
 }
 
 function init() {
-    // Get canvas and context 
+    // Get canvas and context     
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    // Start game    
+    // Create player and food   
+    player = new Rectangle(40, 40, 10, 10);
+    food = new Rectangle(80, 80, 10, 10);
+    // Start game  
     run();
     repaint();
 }
+
+function Rectangle(x, y, width, height) {
+    this.x = (x == null) ? 0 : x;
+    this.y = (y == null) ? 0 : y;
+    this.width = (width == null) ? 0 : width;
+    this.height = (height == null) ? this.width : height;
+    this.intersects = function(rect) { if (rect == null) { window.console.warn('Missing parameters on function intersects'); } else { return (this.x < rect.x + rect.width && this.x + this.width > rect.x && this.y < rect.y + rect.height && this.y + this.height > rect.y); } };
+    this.fill = function(ctx) { if (ctx == null) { window.console.warn('Missing parameters on function fill'); } else { ctx.fillRect(this.x, this.y, this.width, this.height); } };
+}
+
+function random(max) { return Math.floor(Math.random() * max); }
+window.addEventListener('load', init, false);
