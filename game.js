24b29dede1d4b536a7503cfae886d88fc1c94f63
smrk <rodriguezsm72@gmail.com>
2020-11-05 13:15:57 -0300

    Index file modified

--- game.js
+++ game.js
@@ -1,9 +1,13 @@
 var canvas = null,
     ctx = null;
+var x = 50,
+    y = 50;
 
 function paint(ctx) {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
     ctx.fillStyle = '#0f0';
-    ctx.fillRect(50, 50, 100, 60);
+    ctx.fillRect(x, y, 10, 10);
 }
 
 function init() {
